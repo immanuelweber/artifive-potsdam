143becc2245eb8e89e6e6345caf3be2b026c93f0
Immanuel Weber <immanuel.weber@hs-koblenz.de>
2021-07-13 18:42:33 +0200

    add text drawing

--- visualization.py
+++ visualization.py
@@ -1,5 +1,24 @@
 import matplotlib.patches as patches
 import matplotlib.patheffects as patheffects
+import torch
+
+
+def is_list_like(obj):
+    if isinstance(obj, torch.Tensor):
+        return obj.dim() > 0
+    elif isinstance(obj, str):
+        return 0
+    is_iter_colection = hasattr(obj, "__iter__") or hasattr(obj, "__getitem__")
+    has_len = hasattr(obj, "__len__")
+    return is_iter_colection and has_len
+
+
+def listify(obj):
+    if is_list_like(obj):
+        return obj
+    if obj is None:
+        return []
+    return [obj]
 
 
 def draw_outline(plot_object, lw, color="black"):
@@ -51,4 +70,23 @@ def draw_rectangles(
         draw_rectangle(
             axis, r, color, lw, outline_color, outline_lw, fill, fillcolor, linestyle
         )
+    return axis
+
+
+def draw_text(
+    axis, xy, text, size=14, color="white", outline_color="black", outline_lw=1
+):
+    text_object = axis.text(
+        *xy, text, verticalalignment="top", color=color, fontsize=size, weight="bold"
+    )
+    draw_outline(text_object, outline_lw, outline_color)
+    text_object.set_clip_on(True)
+
+
+def draw_texts(
+    axis, texts, positions, size=14, color="white", outline_lw=4, outline_color="black"
+):
+    texts = listify(texts)
+    for t, p in zip(texts, positions):
+        draw_text(axis, p, t, size, color, outline_color, outline_lw)
     return axis
